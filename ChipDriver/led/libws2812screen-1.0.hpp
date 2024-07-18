90bdb0af52593b03258694334c070f4a648f61a7
Boyu.Jin <qianwan233@foxmail.com>
2024-07-18 20:18:18 +0800

    Add more details about imu

--- ChipDriver/led/libws2812screen-1.0.hpp
+++ ChipDriver/led/libws2812screen-1.0.hpp
@@ -17,6 +17,7 @@ namespace Screen {
 
     static uint8_t RED[3] = {255, 0, 0};
     static uint8_t GREEN[3] = {0, 255, 0};
+    static uint8_t YELLOW[3] = {255, 255, 0};
     static uint8_t BLUE[3] = {0, 0, 255};
     static uint8_t BLACK[3] = {0, 0, 0};
     static uint8_t WHITE[3] = {255, 255, 255};
@@ -29,26 +30,32 @@ namespace Screen {
         uint32_t _pixel_width;
         SPIA::cSPIA *_spi = nullptr;
     public:
-        Display(SPIA::cSPIA *pspi, uint8_t *frame_buff, uint32_t frame_buff_len, uint32_t pixel_width,
-                uint32_t pixel_high) : _pixel_high(pixel_high), _pixel_width(pixel_width), _frame_buff(frame_buff),
-                                       _spi(pspi), _frame_buff_len(frame_buff_len) {
-            if (frame_buff != nullptr) {
-                _frame_buff = frame_buff;
-            }
-            if (pspi != nullptr) {
-                _spi = pspi;
-            }
-        }
+        /*
+         * @param pspi: SPI handle
+         * @param frame_buff: Frame buffer = 9*PixelWidth*PixelHigh+2
+         * @param pixel_width: Pixel width
+         * */
+        Display(SPIA::cSPIA *pspi, uint8_t *frame_buff, uint32_t pixel_width,
+                uint32_t pixel_high);
+
+        Display() = default;
+
+        uint8_t ResetScreen(SPIA::cSPIA *pspi, uint8_t *frame_buff, uint32_t pixel_width,
+                            uint32_t pixel_high);
 
         uint8_t SetPixel(uint32_t x, uint32_t y, uint8_t *rgb);
 
         void SetAllPixel(uint8_t *rgb);
+
+        void SetAllPixel(uint8_t *rgb, float brightness);
 
         void Refresh();
 
         uint32_t GetWidth() { return _pixel_width; }
 
         uint32_t GetHigh() { return _pixel_high; }
+
+        virtual void AsynchBuff();
     };
 }
 #endif
